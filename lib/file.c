feb1e8bf06cc784ec358b2b07b02f91f967bc3c1
arty <art.yerkes@gmail.com>
2019-03-10 13:21:57 -0700

    Basic output from idris code

--- lib/file.c
+++ lib/file.c
@@ -3,14 +3,40 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+uint16_t *screen_start = (uint16_t *)0xb8000;
+uint16_t *screen = (uint16_t *)0xb8000;
+uint16_t *screen_end = ((uint16_t *)0xb8000) + (80 * 25);
+
 int open(const char *name, int access, ...) {
     errno = ENOENT;
     return -1;
 }
 
+void scroll() {
+    memcpy(screen_start + 80, screen_start, (screen_end - screen_start - 80) * sizeof(uint16_t));
+    screen -= 80;
+}
+
+void outchar(const char ch) {
+    if (ch == 10) {
+        int col = (screen - screen_start) % 80;
+        screen += 80 - col;
+    } else {
+        *screen++ = 0x1a00 | (0xff & ch);
+    }
+    if (screen >= screen_end) {
+        scroll();
+    }
+}
+
 int write(int fd, const char *data, size_t len) {
-    errno = EBUSY;
-    return -1;
+    if (fd == 1 || fd == 2) {
+        const char *end = data + len;
+        while (data < end) {
+            outchar(*data++);
+        }
+    }
+    return len;
 }
 
 int close(int fd) {
